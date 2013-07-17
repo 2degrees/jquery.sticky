c4d379f4799fab9c8663107a416602ff87976413
Euan Goddard <euangoddard@2degreesnetwork.com>
2013-07-17 10:04:27 +0100

    Fixed behaviour of the sticky element when parent(s) are relative
    Closes #3

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -22,11 +22,6 @@
     
     var NON_DEFAULT_FLOW_POSITIONS = ['relative', 'absolute'];
     
-    var is_element_removed_from_document_flow = function ($element) {
-        var element_position = $element.css('position');
-        return $.inArray(element_position, NON_DEFAULT_FLOW_POSITIONS) !== -1;
-    };
-    
     var $window = $(window);
     var self = {
         init: function (options) {
@@ -49,12 +44,22 @@
                     var $actual_parent_elem = $parent_elem;
                 }
                 
-                $window.bind('resize.sticky, scroll.sticky', function () {
-                    self._handle_scroll.apply(
-                        $sticky_elem, [$parent_elem, $actual_parent_elem, settings]);
+                var $anchor = $('<div />');
+                $anchor.css({
+                    position: 'static',
+                    visibility: 'hidden',
+                    height: 0,
+                    margin: 0,
+                    padding: 0
                 });
-                self._handle_scroll.apply(
-                    $sticky_elem, [$parent_elem, $actual_parent_elem, settings]);
+                $anchor.insertBefore($sticky_elem);
+                
+                var args =
+                    [$parent_elem, $actual_parent_elem, $anchor, settings];
+                $window.bind('resize.sticky, scroll.sticky', function () {
+                    self._handle_scroll.apply($sticky_elem, args);
+                });
+                self._handle_scroll.apply($sticky_elem, args);
             });
             
         },
@@ -63,45 +68,19 @@
             $window.unbind('.sticky');
         },
         
-        _handle_scroll: function ($parent_elem, $actual_parent_elem, settings) {
-            // Compute parameters needed for the sticky element
+        _handle_scroll: function ($parent_elem, $actual_parent_elem, $anchor, settings) {
             var $sticky_elem = $(this);
-            var sticky_elem_margin_x =
-                parseInt($sticky_elem.css('margin-left') || 0, 10) +
-                parseInt($sticky_elem.css('margin-right') || 0, 10);
-            var sticky_elem_border_x =
-                parseInt($sticky_elem.css('border-left-width') || 0, 10) +
-                parseInt($sticky_elem.css('border-right-width') || 0, 10);
             var sticky_elem_height = $sticky_elem.outerHeight();
             
-            // Try to use the previous sibling to reference the top point
-            var $top_elem = $sticky_elem.prevAll();
+            var is_element_smaller_than_window =
+                $window.height() > sticky_elem_height;
             
-            // If there is no previous element, fall back to the parent
-            if ($top_elem.length) {
-                var first_element_offset = null;
-                var total_height = 0;
-                $top_elem.each(function (key, object) {
-                    if (object.nodeName !== 'SCRIPT' &&
-                        object.nodeName !== 'STYLE'
-                        ) {
-                        if (first_element_offset === null) {
-                            first_element_offset = $(object).offset().top;
-                        }
-                        total_height += $(object).height();
-                    }
-                });
-                // Add the the total height = first child's offset top
-                var top_scroll_point = total_height + first_element_offset;
-            } else {
-                $top_elem = $parent_elem;
-                var top_scroll_point = $top_elem.offset().top;
-            }
-            
-            // Decide which display mode sticky element needs to be in
+            var top_scroll_point = $anchor.offset().top;
             var window_scroll_top = $window.scrollTop();
-            if ($window.height() > sticky_elem_height &&
-                window_scroll_top >= (top_scroll_point - settings.gutter)) {
+            var is_element_top_off_screen = 
+                window_scroll_top >= (top_scroll_point - settings.gutter);
+            
+            if (is_element_smaller_than_window && is_element_top_off_screen) {
                 // Determine where the bottom of the element will be if we use
                 // position fixed, as we don't want it to spill over the bottom
                 // of the container
@@ -111,29 +90,33 @@
                 var parent_bottom = $parent_elem.offset().top +
                     $parent_elem.outerHeight() - settings.gutter;
                 
+                var bounding_width = get_element_bounding_width(
+                    $sticky_elem,
+                    $actual_parent_elem,
+                    settings
+                );
                 
-                var fixed_width = settings.width || (
-                    $actual_parent_elem.width() -
-                    sticky_elem_margin_x -
-                    sticky_elem_border_x
-                    );
-                
-                
-                if (sticky_elem_potential_bottom >= (parent_bottom - settings.gutter)) {
+                var bottom_cut_off = parent_bottom - settings.gutter;
+                if (sticky_elem_potential_bottom >= bottom_cut_off) {
                     if (is_element_removed_from_document_flow($actual_parent_elem)) {
                         parent_bottom -= $parent_elem.offset().top;
                     }
                     $sticky_elem.css({
                         position: 'absolute',
-                        top: parent_bottom - sticky_elem_height,
-                        width: fixed_width
-                        });
+                        top: (parent_bottom - sticky_elem_height) + 'px',
+                        width: bounding_width + 'px',
+                        left: 'auto'
+                    });
                     
                 } else {
+                    var fixed_left =
+                        $anchor.offset().left - $window.scrollLeft();
                     $sticky_elem.css({
                         position: 'fixed',
                         top: settings.gutter + 'px',
-                        width: fixed_width});
+                        left: fixed_left + 'px',
+                        width: bounding_width + 'px'
+                    });
                 }
                 
             } else {
@@ -149,8 +132,40 @@
             return self.init.apply(this, arguments);
         } else {
             $.error('Method ' + method + ' does not exist on jQuery.sticky');
+        } 
+    };
+    
+    var is_element_removed_from_document_flow = function ($element) {
+        var element_position = $element.css('position');
+        return $.inArray(element_position, NON_DEFAULT_FLOW_POSITIONS) !== -1;
+    };
+    
+    var sum_css_sizes = function ($element, css_propery_names) {
+        var sum = 0;
+        $.each(css_propery_names, function (index, css_property_name) {
+            sum += parseInt($element.css(css_property_name) || 0, 10);
+        });
+        return sum;
+    };
+    
+    var get_element_bounding_width = function ($element, $container, settings) {
+        var width;
+        if (settings.width) {
+            width = settings.width;
+        } else {
+            var element_margin_x = sum_css_sizes(
+                $element,
+                ['margin-left', 'margin-right']
+            );
+            var element_border_x = sum_css_sizes(
+                $element,
+                ['border-left-width', 'border-right-width']
+            );
+            
+            width = $container.width() - element_margin_x - element_border_x;
         }
-    }
+        return width;
+    };
     
 })(jQuery);
 
